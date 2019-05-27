a711f108e539139cfd2741aa76a8a3273d4175c3
georgyogolla <georgyogolla@gmail.com>
2019-05-27 16:22:07 +0300

    composing different components in React

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+const contentNode = document.getElementById('contents');
+class IssueFilter extends React.Component{
+    render(){
+    return(
+        <div>This is a placeholder for the Issue Filter.</div>
+    )
+    }
+
+};
+ class IssueTable extends React.Component{
+     render(){
+         return(
+             <div>This is a placehoder for the Issue Table</div>
+         )
+     }
+ };
+ class IssueAdd extends React.Component{
+     render(){
+     return(
+         <div>This is a placeholder for the Issue Add entry form.</div>
+     )
+     }
+ };
+
+class IssueList extends React.Component {
+    render() {
+      return (
+          <div>
+        <h1>Issue Tracker</h1>,
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+        </div>
+      );
+    }
+  } 
+  ReactDOM.render(<IssueList />, contentNode);
